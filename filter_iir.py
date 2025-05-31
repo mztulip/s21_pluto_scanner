83ceaf1ed7af35eaca88d87eb5d17ad0897870ff
Mateusz <mateusz@tulip.lol>
2025-05-31 15:10:25 +0200

    filter testing, added code with example sinusoids filtering

--- filter_iir.py
+++ filter_iir.py
@@ -38,32 +38,67 @@ def mfreqz(b,a=1 ):
     subplots_adjust(hspace=0.5)
 mfreqz(b_fir)
 
-show()
+# show()
 
-# #------------------------------------------------
-# # Plot the original and filtered signals.
-# #------------------------------------------------
+#------------------------------------------------
+# Plot the original and filtered signals.
+#------------------------------------------------
+base_freq = 100_000
+period_base_freq = 1.0/base_freq
+sampling_period = 1.0/sample_rate
+nsamples = 500* period_base_freq/sampling_period
+print(f"Filering example samples count: {nsamples}")
 
-# nsamples = 800
-# base_freq = 100_000
-# t = np.arange(nsamples) / sample_rate
+if nsamples < N:
+    print("Warning input signal samples count too low")
+t = np.arange(nsamples) / sample_rate
 
-# x = 200*np.sin(2*np.pi*1_000_000*t)
+x = (2^12)*np.sin(2*np.pi*1_000_000*t)
 
-# # Use lfilter to filter x with the FIR filter.
+# Use lfilter to filter x with the FIR filter.
 # filtered_x = lfilter(b = b,a = a,x = x)
 
+filtered_x = fftconvolve(x, b_fir, mode='same')
+
 # # The phase delay of the filtered signal.
-# delay = 0.5 * (M+N-1) / sample_rate
+delay = 0.5 * (N-1) / sample_rate
 
+figure(3)
+# Plot the original signal.
+plot(t, x)
+# Plot the filtered signal, shifted to compensate for the phase delay.
+plot(t, filtered_x, 'r-')
+# Plot just the "good" part of the filtered signal.  The first N-1
+# samples are "corrupted" by the initial conditions.
+# plot(t[N-1:]-delay, filtered_x[N-1:], 'g', linewidth=4)
 
-# figure(3)
-# # Plot the original signal.
-# plot(t, x)
-# # Plot the filtered signal, shifted to compensate for the phase delay.
-# plot(t-delay, filtered_x, 'r-')
-# # Plot just the "good" part of the filtered signal.  The first N-1
-# # samples are "corrupted" by the initial conditions.
-# plot(t[M+N-1:]-delay, filtered_x[M+N-1:], 'g', linewidth=4)
+base_freq = 100
+period_base_freq = 1.0/base_freq
+sampling_period = 1.0/sample_rate
+nsamples = 100* period_base_freq/sampling_period
+print(f"Filering example samples count: {nsamples}")
 
-# show()
+if nsamples < 2* N:
+    print("Warning input signal samples count too low")
+t = np.arange(nsamples) / sample_rate
+
+x = (2^12)*np.sin(2*np.pi*base_freq*t)
+
+# Use lfilter to filter x with the FIR filter.
+# filtered_x = lfilter(b = b,a = a,x = x)
+
+filtered_x = fftconvolve(x, b_fir, mode='same')
+
+# # The phase delay of the filtered signal.
+delay = 0.5 * (N-1) / sample_rate
+
+figure(4)
+# Plot the original signal.
+plot(t, x)
+# Plot the filtered signal, shifted to compensate for the phase delay.
+plot(t, filtered_x, 'r-')
+# Plot just the "good" part of the filtered signal.  The first N-1
+# samples are "corrupted" by the initial conditions.
+# plot(t[N-1:]-delay, filtered_x[N-1:], 'g', linewidth=4)
+
+show()
